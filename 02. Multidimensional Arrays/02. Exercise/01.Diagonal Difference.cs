2ed572d230af39e1eda727e0c29682ee6f5729ae
VakrilZapryanov <vakrill.zapryanov@gmail.com>
2025-10-05 21:09:21 +0300

    Advanced

---
+++ 02. Multidimensional Arrays/02. Exercise/01.Diagonal Difference.cs
@@ -0,0 +1,39 @@
+﻿int size = int.Parse(Console.ReadLine());
+
+int[,] matrix = ReadMatrix(size);
+
+int primaryDiagonalSum = default;
+for (int row = 0; row < size; row++)
+{
+    int col = row;
+
+    primaryDiagonalSum += matrix[row, col];
+}
+
+int secondaryDiagonalSum = default;
+for (int row = 0; row < size; row++)
+{
+    secondaryDiagonalSum += matrix[row, size - row - 1];
+}
+
+Console.WriteLine(Math.Abs(primaryDiagonalSum - secondaryDiagonalSum));
+
+static int[,] ReadMatrix(int size)
+{
+    var matrix = new int[size, size];
+
+    for (int row = 0; row < size; row++)
+    {
+        int[] currentRow = Console.ReadLine()
+            .Split()
+            .Select(int.Parse)
+            .ToArray();
+
+        for (int col = 0; col < size; col++)
+        {
+            matrix[row, col] = currentRow[col];
+        }
+    }
+
+    return matrix;
+}
