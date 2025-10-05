db77dd1fcc37e99d04ca959e9fb8a575d78646fb
VakrilZapryanov <vakrill.zapryanov@gmail.com>
2025-10-05 21:30:57 +0300

    Advanced

---
+++ 02. Multidimensional Arrays/02. Exercise/02.Squares in Matrix.cs
@@ -0,0 +1,41 @@
+﻿int[] matrixDimensions = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+int rows = matrixDimensions[0], cols = matrixDimensions[1];
+
+string[,] matrix = ReadMatrix(rows, cols);
+
+int squaresCount = default;
+for (int row = 0; row < rows - 1; row++)
+{
+    for (int col = 0; col < cols - 1; col++)
+    {
+        if (matrix[row, col] == matrix[row, col + 1] &&
+            matrix[row, col] == matrix[row + 1, col] &&
+            matrix[row, col] == matrix[row + 1, col + 1])
+        {
+            squaresCount++;
+        }
+    }
+}
+
+Console.WriteLine(squaresCount);
+
+static string[,] ReadMatrix(int rows, int cols)
+{
+    var matrix = new string[rows, cols];
+
+    for (int row = 0; row < rows; row++)
+    {
+        string[] currentRow = Console.ReadLine().Split();
+
+        for (int col = 0; col < cols; col++)
+        {
+            matrix[row, col] = currentRow[col];
+        }
+    }
+
+    return matrix;
+}
