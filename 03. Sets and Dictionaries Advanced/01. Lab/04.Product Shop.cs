328577decb1f413f6f1f5451a855c256e3e372b3
VakrilZapryanov <vakrill.zapryanov@gmail.com>
2025-10-14 23:02:30 +0300

    Advanced

---
+++ 03. Sets and Dictionaries Advanced/01. Lab/04.Product Shop.cs
@@ -0,0 +1,24 @@
+﻿SortedDictionary<string, Dictionary<string, double>> stores = new();
+
+string command = string.Empty;
+while ((command = Console.ReadLine()) != "Revision")
+{
+    string[] arguments = command.Split(", ");
+
+    string shopName = arguments[0];
+    string product = arguments[1];
+    double productPrice = double.Parse(arguments[2]);
+
+    if (!stores.ContainsKey(shopName))
+        stores[shopName] = new Dictionary<string, double>();
+
+    stores[shopName][product] = productPrice;
+}
+
+foreach (var (store, products) in stores)
+{
+    Console.WriteLine(store + "->");
+
+    foreach (var (product, price) in products)
+        Console.WriteLine($"Product: {product}, Price: {price}");
+}
