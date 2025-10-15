607a4487727c3ac7e9da2dd55ca5da8595e133fb
VakrilZapryanov <vakrill.zapryanov@gmail.com>
2025-10-15 07:15:44 +0300

    Advanced

---
+++ 03. Sets and Dictionaries Advanced/01. Lab/02.Average Student Grades.cs
@@ -0,0 +1,25 @@
+﻿int gradesCount = int.Parse(Console.ReadLine());
+
+Dictionary<string, List<decimal>> studentRecords = new();
+
+for (int i = 0; i < gradesCount; i++)
+{
+    string[] currentStudentInformation = Console.ReadLine().Split();
+
+    string name = currentStudentInformation[0];
+    decimal grade = decimal.Parse(currentStudentInformation[1]);
+
+    if (!studentRecords.ContainsKey(name))
+        studentRecords[name] = new List<decimal>();
+
+    studentRecords[name].Add(grade);
+}
+
+foreach (var (name, grades) in studentRecords)
+{
+    var fixedGrades = grades.Select(gr => gr.ToString("F2"));
+
+    var joinedGrades = string.Join(" ", fixedGrades);
+
+    Console.WriteLine($"{name} -> {joinedGrades} (avg: {grades.Average():f2})");
+}
