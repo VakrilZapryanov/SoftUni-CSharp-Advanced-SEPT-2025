a3cc9c95bcc4e2c85886e26ff974081d37a9636a
VakrilZapryanov <vakrill.zapryanov@gmail.com>
2025-10-15 20:59:59 +0300

    Advanced

---
+++ 03. Sets and Dictionaries Advanced/02. Exercise/01.Unique Usernames.cs
@@ -0,0 +1,18 @@
+﻿int usernamesCount = int.Parse(Console.ReadLine());
+
+HashSet<string> uniqueUsernames = new();
+List<string> uniqueInsertOrder = new();
+
+for (int i = 0; i < usernamesCount; i++)
+{
+    string currentUsername = Console.ReadLine();
+
+    if (!uniqueUsernames.Contains(currentUsername))
+    {
+        uniqueUsernames.Add(currentUsername);
+        uniqueInsertOrder.Add(currentUsername);
+    }
+}
+
+foreach (var username in uniqueInsertOrder)
+    Console.WriteLine(username);
