8917b130826f4464713699d6e4b6a673b09ba4a9
VakrilZapryanov <vakrill.zapryanov@gmail.com>
2025-10-15 06:57:51 +0300

    Advanced

---
+++ 03. Sets and Dictionaries Advanced/01. Lab/05.Cities by Continent and Country.cs
@@ -0,0 +1,31 @@
+﻿int inputCount = int.Parse(Console.ReadLine());
+
+Dictionary<string, Dictionary<string, List<string>>> continents = new();
+
+while (inputCount > 0)
+{
+    inputCount--;
+
+    string[] information = Console.ReadLine().Split();
+
+    string continent = information[0];
+    string country = information[1];
+    string city = information[2];
+
+    if (!continents.ContainsKey(continent))
+        continents[continent] = new Dictionary<string, List<string>>();
+
+    if (!continents[continent].ContainsKey(country))
+        continents[continent][country] = new List<string>();
+
+    continents[continent][country].Add(city);
+
+}
+
+foreach (var (continent, countries) in continents)
+{
+    Console.WriteLine(continent + ":");
+
+    foreach (var (country, cities) in countries)
+        Console.WriteLine($"    {country} -> {string.Join(", ", cities)}");
+}
