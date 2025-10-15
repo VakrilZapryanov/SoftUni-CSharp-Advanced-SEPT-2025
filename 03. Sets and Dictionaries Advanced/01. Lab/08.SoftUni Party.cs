857c607d42e02a68a23ae732edaf964d92349f7b
VakrilZapryanov <vakrill.zapryanov@gmail.com>
2025-10-15 20:47:48 +0300

    Advanced

---
+++ 03. Sets and Dictionaries Advanced/01. Lab/08.SoftUni Party.cs
@@ -0,0 +1,40 @@
+﻿HashSet<string> vip = new();
+HashSet<string> regular = new();
+
+bool hasParty = false;
+string command = string.Empty;
+while ((command = Console.ReadLine()) != "END")
+{
+    if (command == "PARTY")
+    {
+        hasParty = true;
+        break;
+    }
+
+    char firstChar = command[0];
+
+    if (firstChar >= 48 && firstChar <= 57)
+        vip.Add(command);
+    else
+        regular.Add(command);
+}
+
+while (hasParty && (command = Console.ReadLine()) != "END")
+{
+    char firstChar = command[0];
+
+    if (firstChar >= 48 && firstChar <= 57)
+        vip.Remove(command);
+    else
+        regular.Remove(command);
+}
+
+int remainingReservations = vip.Count + regular.Count;
+
+Console.WriteLine(remainingReservations);
+
+foreach (var vipReservation in vip)
+    Console.WriteLine(vipReservation);
+
+foreach (var regularReservation in regular)
+    Console.WriteLine(regularReservation);
