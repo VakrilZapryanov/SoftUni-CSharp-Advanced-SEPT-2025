663dbb087400d3f63e1a39a78d758d6e66f5406f
VakrilZapryanov <vakrill.zapryanov@gmail.com>
2025-10-15 18:17:57 +0300

    Advanced

---
+++ 03. Sets and Dictionaries Advanced/01. Lab/06.Record Unique Names.cs
@@ -0,0 +1,13 @@
+﻿int namesCount = int.Parse(Console.ReadLine());
+
+HashSet<string> uniqueNames = new();
+
+for (int i = 0; i < namesCount; i++)
+{
+    string name = Console.ReadLine();
+
+    uniqueNames.Add(name);
+}
+
+foreach (var name in uniqueNames)
+    Console.WriteLine(name);
