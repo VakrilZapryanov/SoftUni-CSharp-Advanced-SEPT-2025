f640316b61b5cfcdf9c0541cba9a60427a74c3f5
VakrilZapryanov <vakrill.zapryanov@gmail.com>
2025-10-15 20:13:05 +0300

    Advanced

---
+++ 03. Sets and Dictionaries Advanced/01. Lab/07.Parking Lot.cs
@@ -0,0 +1,31 @@
+﻿HashSet<string> parking = new();
+
+string command = string.Empty;
+while ((command = Console.ReadLine()) != "END")
+{
+    string[] arguments = command.Split(", ");
+
+    string direction = arguments[0];
+    string carNumber = arguments[1];
+
+    switch (direction)
+    {
+        case "IN":
+            parking.Add(carNumber);
+
+            break;
+        case "OUT":
+            if (parking.Contains(carNumber))
+                parking.Remove(carNumber);
+
+            break;
+    }
+}
+
+if (parking.Any())
+{
+    foreach (var car in parking)
+        Console.WriteLine(car);
+}
+else
+    Console.WriteLine("Parking Lot is Empty");
